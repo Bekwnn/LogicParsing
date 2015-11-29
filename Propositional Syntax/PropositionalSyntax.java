0ee0e65f75b79bf6e19cbfbe904c46c6f1fbb36d
unknown <e.w.nickerson@gmail.com>
2015-11-28 16:20:25 -0800

    propositional evaluation working, needs to read from file still

--- Propositional Syntax/PropositionalSyntax.java
+++ Propositional Syntax/PropositionalSyntax.java
@@ -1,59 +1,63 @@
 import java.util.Stack;
-import java.util.Map;
+import java.util.HashMap;
 
 public class PropositionalSyntax
 {
 	enum EOperType {
-		DIMPLY, //double imply
-		IMPLY,
-		AND,
-		OR,
-		NOT,
-		ISNTOPER //isn't an operator
+		eDIMPLY, //double imply
+		eIMPLY,
+		eAND,
+		eOR,
+		eNOT,
+		eISNTOPER //isn't an operator
 	}
-	public static final String DIMPLY = '=';
-	public static final String IMPLY = '>';
-	public static final String AND = '&';
-	public static final String OR = '|';
-	public static final String NOT = '!';
+	public static final String DIMPLY = "=";
+	public static final String IMPLY = ">";
+	public static final String AND = "&";
+	public static final String OR = "|";
+	public static final String NOT = "!";
 	
 	public static void main(String[] args)
 	{
-		Map<String, boolean> myMap = new Map<String, boolean>();
-		
+		HashMap<String, Boolean> myMap = new HashMap<String, Boolean>();
+		myMap.put("A", true);
+		myMap.put("B", false);
+		myMap.put("C", false);
+		System.out.println("Result: " + evalExpression("! & & A B C", myMap));
 	}
 	
-	static boolean evalExpression(String expression, Map<String, boolean> values)
+	static boolean evalExpression(String expression, HashMap<String, Boolean> values)
 	{
-		Stack<boolean> valueStack = new Stack<boolean>();
-		String[] tokenized = expression.split();
-		for (int i = tokenized.length-1; i > 0; i--;)
+		Stack<Boolean> valueStack = new Stack<Boolean>();
+		String[] tokenized = expression.split(" ");
+		for (int i = tokenized.length-1; i >= 0; i--)
 		{
 			EOperType operation = operType(tokenized[i]);
 			//if it's not an operation, get value from map and push onto stack
-			if (operation == EOperType.ISNTOPER)
+			if (operation == EOperType.eISNTOPER)
 			{
 				boolean temp = values.get(tokenized[i]);
 				valueStack.push(temp);
+				System.out.println("Pushed " + tokenized[i] + " onto stack.");
 			}
 			else
 			{
 				boolean temp; //store our result
-				switch(operation):
+				switch(operation)
 				{
-					case DIMPLY:
+					case eDIMPLY:
 						temp = operDIMPLY(valueStack.pop(), valueStack.pop());
 						break;
-					case IMPLY:
+					case eIMPLY:
 						temp = operIMPLY(valueStack.pop(), valueStack.pop());
 						break;
-					case AND:
+					case eAND:
 						temp = operAND(valueStack.pop(), valueStack.pop());
 						break;
-					case OR:
+					case eOR:
 						temp = operOR(valueStack.pop(), valueStack.pop());
 						break;
-					case NOT:
+					case eNOT:
 						temp = operNOT(valueStack.pop());
 						break;
 					default:
@@ -64,41 +68,47 @@ public class PropositionalSyntax
 				valueStack.push(temp);
 			}
 		}
+		return valueStack.pop();
 	}
 	
 	static EOperType operType(String token)
 	{
-		if      (token.equals(DIMPLY)) return EOperType.DIMPLY;
-		else if (token.equals(IMPLY))  return EOperType.IMPLY;
-		else if (token.equals(AND))    return EOperType.AND;
-		else if (token.equals(OR))     return EOperType.OR;
-		else if (token.equals(NOT))    return EOperType.NOT;
+		if      (token.equals(DIMPLY)) return EOperType.eDIMPLY;
+		else if (token.equals(IMPLY))  return EOperType.eIMPLY;
+		else if (token.equals(AND))    return EOperType.eAND;
+		else if (token.equals(OR))     return EOperType.eOR;
+		else if (token.equals(NOT))    return EOperType.eNOT;
 		
-		else return ISNTOPER;
+		else return EOperType.eISNTOPER;
 	}
 	
 	static boolean operAND(boolean a, boolean b)
 	{
+		System.out.println("Performing AND");
 		return a && b;
 	}
 	
 	static boolean operOR(boolean a, boolean b)
 	{
+		System.out.println("Performing OR");
 		return a || b;
 	}
 	
 	static boolean operNOT(boolean a)
 	{
+		System.out.println("Performing NOT");
 		return !a;
 	}
 	
 	static boolean operIMPLY(boolean a, boolean b)
 	{
+		System.out.println("Performing IMPLY");
 		return operOR(operNOT(a), b);
 	}
 	
 	static boolean operDIMPLY(boolean a, boolean b)
 	{
+		System.out.println("Performing DIMPLY");
 		return operAND(operIMPLY(a, b), operIMPLY(b, a));
 	}
 }
